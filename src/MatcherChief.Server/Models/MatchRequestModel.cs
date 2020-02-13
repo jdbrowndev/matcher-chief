30d8ed1663750f59cb32106105bf3e203f4fca49
Jordan Brown <emagdne@gmail.com>
2020-02-12 22:32:57 -0500

    websocket request handling

--- src/MatcherChief.Server/Models/MatchRequestModel.cs
+++ src/MatcherChief.Server/Models/MatchRequestModel.cs
@@ -1,7 +1,10 @@
+using MatcherChief.Core.Models;
+
 namespace MatcherChief.Server.Models
 {
     public class MatchRequestModel
     {
+        public GameFormat Format { get; set; }
         // TODO: implement
     }
 }
--- src/MatcherChief.Server/WebSockets/WebSocketRequestHandler.cs
+++ src/MatcherChief.Server/WebSockets/WebSocketRequestHandler.cs
@@ -1,5 +1,11 @@
+using System;
 using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
+using MatcherChief.Server.Models;
+using MatcherChief.Server.Queues;
 
 namespace MatcherChief.Server.WebSockets
 {
@@ -10,10 +16,36 @@ namespace MatcherChief.Server.WebSockets
 
     public class WebSocketRequestHandler : IWebSocketRequestHandler
     {
-        public async Task Handle(WebSocket socket, TaskCompletionSource<object> tcs)
+        private readonly IQueueManager _queueManager;
+
+        public WebSocketRequestHandler(IQueueManager queueManager)
         {
-            // TODO: implement
-            await Task.Delay(1000);
+            _queueManager = queueManager;
+        }
+
+        public async Task Handle(WebSocket webSocket, TaskCompletionSource<object> tcs)
+        {
+            var sb = new StringBuilder();
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult receiveResult;
+
+            do
+            {
+                receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                sb.Append(Encoding.UTF8.GetString(buffer));
+            } while (!receiveResult.CloseStatus.HasValue);
+
+            var request = sb.ToString();
+            var model = JsonSerializer.Deserialize<MatchRequestModel>(request);
+
+            var queue = _queueManager.GameFormatsToQueues[model.Format];
+            var queuedMatchRequest = new QueuedMatchRequestModel
+            {
+                WebSocket = webSocket,
+                CompletionSource = tcs
+                // TODO: assign more props
+            };
+            queue.Add(queuedMatchRequest);
         }
     }
 }
