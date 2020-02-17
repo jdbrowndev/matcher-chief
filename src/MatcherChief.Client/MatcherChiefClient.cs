5d88ea07f288e20a09d8e944e0a1875dc5677235
Jordan Brown <emagdne@gmail.com>
2020-02-16 21:33:40 -0500

    add initial client code

---
+++ src/MatcherChief.Client/MatcherChiefClient.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+using MatcherChief.Shared;
+using System.Text;
+using System.Linq;
+
+namespace MatcherChief.Client
+{
+    public class MatcherChiefClient
+    {
+        private readonly Uri _matchmakingServer;
+
+        public MatcherChiefClient(Uri matchmakingServer)
+        {
+            _matchmakingServer = matchmakingServer;
+        }
+
+        // TODO: clean up request/response models
+        public async Task<MatchResponseModel> GetMatch(MatchRequestModel request, CancellationToken cancellationToken)
+        {
+            using (var webSocket = new ClientWebSocket())
+            {
+                var json = JsonSerializer.Serialize(request);
+                var bytes = Encoding.UTF8.GetBytes(json);
+                var bufferSize = 1024 * 4;
+                var segments = ArraySegmentHelper.Segment(bytes, bufferSize);
+
+                await webSocket.ConnectAsync(new Uri(_matchmakingServer, "/ws"), cancellationToken);
+                foreach (var segment in segments)
+                {
+                    var eom = segment == segments.Last();
+                    await webSocket.SendAsync(segment, WebSocketMessageType.Text, eom, CancellationToken.None);
+                }
+
+                var sb = new StringBuilder();
+                var buffer = new byte[1024 * 4];
+                WebSocketReceiveResult receiveResult;
+
+                do
+                {
+                    receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
+                    sb.Append(Encoding.UTF8.GetString(buffer));
+                } while (!receiveResult.CloseStatus.HasValue);
+
+                var response = sb.ToString();
+                var model = JsonSerializer.Deserialize<MatchResponseModel>(response);
+
+                // TODO: close webSocket?
+
+                return model;
+            }
+        }
+    }
+}
--- src/MatcherChief.Server/WebSockets/WebSocketResponseHandler.cs
+++ src/MatcherChief.Server/WebSockets/WebSocketResponseHandler.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
