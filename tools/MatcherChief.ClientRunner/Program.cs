f7abcf8bfb954a7ebc828d76cfb24e0ca1551c25
Jordan Brown <emagdne@gmail.com>
2022-04-01 18:18:23 -0400

    update client runner

--- tools/MatcherChief.ClientRunner/Program.cs
+++ tools/MatcherChief.ClientRunner/Program.cs
@@ -15,6 +15,7 @@ public class Program
 {
 	private static Random _random = new Random();
 	private static GameFormat[] _gameFormats = (GameFormat[])Enum.GetValues(typeof(GameFormat));
+	private static int _totalRequests = 0;
 
 	public static async Task Main()
 	{
@@ -24,15 +25,30 @@ public class Program
 
 		Console.WriteLine($"ClientRunner starting up... (ServerUrl={serverUrl}, ConnectionDelay={msDelay})");
 
+		_ = LogProgress();
 		var uri = new Uri(serverUrl);
 		var client = new MatcherChiefClient(uri);
 		var tasks = new List<Task>();
+
 		while (true)
 		{
-			var request = GetRequest();
-			var responseTask = client.GetMatch(request, CancellationToken.None);
-			tasks = tasks.Where(x => !x.IsCompleted).Concat(new[] { responseTask }).ToList();
-			await Task.Delay(msDelay);
+			try
+			{
+				tasks = await RefreshTasks(tasks);
+
+				var request = GetRequest();
+				var responseTask = client.GetMatch(request, CancellationToken.None);
+				tasks.Add(responseTask);
+				_totalRequests++;
+			}
+			catch (Exception exception)
+			{
+				Console.Error.WriteLine(exception);
+			}
+			finally
+			{
+				await Task.Delay(msDelay);
+			}
 		}
 	}
 
@@ -94,5 +110,35 @@ public class Program
 			GameModes = modes
 		};
 		return request;
+	}
+
+	private static async Task<List<Task>> RefreshTasks(List<Task> tasks)
+	{
+		var completedTasks = tasks.Where(x => x.IsCompleted).ToList();
+		var runningTasks = tasks.Where(x => !completedTasks.Contains(x)).ToList();
+
+		foreach (var task in completedTasks)
+		{
+			try
+			{
+				await task;
+			}
+			catch (Exception exception)
+			{
+				Console.Error.WriteLine(exception);
+			}
+		}
+
+		return runningTasks;
+	}
+
+	private static async Task LogProgress()
+	{
+		var timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
+
+		while (await timer.WaitForNextTickAsync())
+		{
+			Console.WriteLine($"Total requests sent: {_totalRequests}");
+		}
 	}
 }
